ce11da177c81da8dbc2d8a5d1097c6306604cf17
Ard Timmerman <ardtimmerman@gmail.com>
2023-05-28 15:40:56 +0200

    feat: add dark mode toggle

---
+++ src/component/dark-mode-toggle.js
@@ -0,0 +1,39 @@
+class DarkModeToggle extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <label class="toggle" for="dark-mode-checkbox">
+        <input type="checkbox" id="dark-mode-checkbox">
+        Dark Mode
+      </label>
+    `;
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    const checkbox = this.shadowRoot.getElementById('dark-mode-checkbox');
+    checkbox.addEventListener('change', DarkModeToggle.toggleDarkMode);
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      checkbox.checked = true;
+      DarkModeToggle.toggleDarkMode({ target: checkbox });
+    }
+  }
+
+  static toggleDarkMode(event) {
+    const checkbox = event.target;
+    const body = document.querySelector('body');
+
+    if (checkbox.checked) {
+      body.style.setProperty('--bg-color', 'black');
+      body.style.setProperty('--text-color', 'white');
+    } else {
+      body.style.setProperty('--bg-color', 'white');
+      body.style.setProperty('--text-color', 'black');
+    }
+  }
+}
+
+customElements.define('c-dark-mode-toggle', DarkModeToggle);
